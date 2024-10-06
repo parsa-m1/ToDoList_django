2b0c3b11d5906d6d1702d9b6a9d78a37fab7599a
parsa-m1 <parsa.work0000@gmail.com>
2024-10-06 17:39:50 +0330

    signup, login and logout added

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('todos_list/', views.todos_list, name='todos_list'),
+
+    path('register/', views.register_page, name='register'),
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_page, name='logout'),
 ]
 
--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Todo
+
+def todos_list(request):
+
+    return render(request, 'todos_list.html')
 
 def index(request):
     todos = Todo.objects_recent.all()
 
     return render(request, 'index.html')
+
+
+def register_page(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            login(request, user)
+            return redirect('todos_list')
+
+    return render(request, 'register.html', {"form": form})
+
+
+def login_page(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('todos_list')
+
+    return render(request, 'login.html')
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('index')
+
+
+
+
+
+
 
 
